13254d64c015e89664d508ee666003a730a04ff8
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-17 12:39:35 +0700

    buat file

---
+++ Controllers/Controller.js
@@ -0,0 +1,11 @@
+const Model = require('../models/index')
+const Student = Model.Student
+const Teacher = Model.Teacher
+const Subject = Model.Subject
+
+
+class Controller {
+
+}
+
+module.exports = Controller
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+const express = require('express')
+let app = express()
+const routes  = require('./routes/home')
+const Student = require('./routes/student')
+const Teacher = require('./routes/teacher')
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use('/',routes)
+app.use('/student' , Student)
+app.use('/teacher' , Teacher)
+
+app.listen(3000, () => {
+  console.log('App listenign to port 3000')
+})
