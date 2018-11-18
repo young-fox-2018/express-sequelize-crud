52c217045f3c1ed0a75148f1b54162258d423065
Zhang <zhang@ZhangMBP15.local>
2018-11-18 10:35:40 +0700

    done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes  = require('./routes')
 const Student = require('./routes/student')
 const Teacher = require('./routes/teacher')
 const bodyParser = require('body-parser')
-
+const port = 3330
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,6 +15,6 @@ app.use('/',routes)
 app.use('/student' , Student)
 app.use('/teacher' , Teacher)
 
-app.listen(3000, () => {
-  console.log('App listenign to port 3000')
+app.listen(port, () => {
+  console.log(`App listening to port ${port}`)
 })
